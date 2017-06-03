e7089e82fd93a18e0e2259bdf60dd295879be05c
bunnycide <andywu1000@gmail.com>
2017-06-03 15:41:00 -0700

    2 robot shenanigans

--- js/robot.js
+++ js/robot.js
@@ -20,7 +20,6 @@ var right = false;
 var left = false;
 var shoot = false;
 var SHOOTDELAY = 0.25;
-var shootTimer;
 var shootDelta;
 var BOUNCE_CONSTANT = 0.8;
 var goal;
@@ -40,6 +39,12 @@ function rectangle(x,y,w,h,vx,vy,color) {
   this.airborne = false;
   this.heading = 1;
   this.ball = 3;
+  this.up = false;
+  this.down = false;
+  this.left = false;
+  this.right = false;
+  this.shoot = false;
+  this.shootTimer = Date.now();
   this.draw = function() {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x,this.y,this.w,this.h);
@@ -72,9 +77,33 @@ function collisionr(circlea, rectb) {
   return circlea.x-circlea.radius<=rectb.x+rectb.w&&circlea.x+circlea.radius>=rectb.x && circlea.y-circlea.radius<=rectb.y+rectb.h&&circlea.y+circlea.radius>=rectb.y;
 }
 
+function collisionl(circlea) {
+  if(circlea.x+circlea.radius>=goal.x&&circlea.x-circlea.radius<=goal.x+goal.w&&circlea.y+circlea.radius>=goal.y&&circlea.y-circlea.radius<=goal.y+goal.w/2) {
+    for(i = 0;i<goal.w/2;i++) {
+      if(circlea.y+circlea.radius>=goal.y+i && circlea.y-circlea.radius<=goal.y+i) {
+        if(circlea.x+circlea.radius>=goal.x+goal.w/2-i && circlea.x-circlea.radius<=goal.x +goal.w/2-i) {
+          return true;
+        }
+        else if(circlea.x+circlea.radius>=goal.x+goal.w/2+i && circlea.x-circlea.radius<=goal.x+goal.w/2+i) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function collisionrr(recta, rectb) {
+  return (recta.x>=rectb.x&&recta.x<=rectb.x+rectb.w||recta.x+recta.w>=rectb.x&&recta.x+recta.w<=rectb.x+rectb.w) && (recta.y>=rectb.y&&recta.y<=rectb.y+rectb.h||recta.y+recta.h>=rectb.y&&recta.y+recta.h<=rectb.y+rectb.h);
+}
+
 function angle(circlea, circleb) {
   //alert(circlea.x-circleb.x > 0 ? Math.atan2(circlea.y-circleb.y, circlea.x-circleb.x) : Math.atan2(circlea.y-circleb.y, circlea.x-circleb.x) + Math.PI);
   return circlea.x-circleb.x > 0 ? Math.atan2(circlea.y-circleb.y, circlea.x-circleb.x) : Math.atan2(circlea.y-circleb.y, circlea.x-circleb.x) + Math.PI;
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function distance(circlea, circleb) {
@@ -97,6 +126,16 @@ function animate()
   ctx.fillText("FPS: " + Math.round(fps), CWIDTH - 60, 10);
   ctx.font = "20px Arial";
   ctx.fillText("Score: " + score,CWIDTH/2,30);
+
+  ctx.fillStyle = "gray";
+  ctx.beginPath();
+  ctx.moveTo(goal.x+goal.w/2,goal.y+1);
+  ctx.lineTo(goal.x,goal.y+goal.w/2);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(goal.x+goal.w/2,goal.y+1);
+  ctx.lineTo(goal.x+goal.w,goal.y+goal.w/2);
+  ctx.stroke();
 
   goal.draw();
 
@@ -121,17 +160,17 @@ function animate()
       robots[i].vx = 0;
     }
 
-    if(!robots[0].airborne) {
-      if(up) {
-        robots[0].vy -= JUMPSPEED;
+    if(!robots[i].airborne) {
+      if(robots[i].up) {
+        robots[i].vy -= JUMPSPEED;
       }
-      if(right) {
-        robots[0].vx += MOVESPEED;
-        robots[0].heading = 1;
+      if(robots[i].right) {
+        robots[i].vx += MOVESPEED;
+        robots[i].heading = 1;
       }
-      if(left) {
-        robots[0].vx -= MOVESPEED;
-        robots[0].heading = -1;
+      if(robots[i].left) {
+        robots[i].vx -= MOVESPEED;
+        robots[i].heading = -1;
       }
     }
 
@@ -165,45 +204,48 @@ function animate()
 
     robots[i].draw();
 
-    if(!shootTimer) {
-      shootTimer = Date.now();
+    if(!robots[i].shootTimer) {
+      robots[i].shootTimer = Date.now();
     }
-    shootDelta = (new Date().getTime() - shootTimer)/1000;
-    if(shoot && robots[i].ball > 0) {
+    shootDelta = (new Date().getTime() - robots[i].shootTimer)/1000;
+    if(robots[i].shoot && robots[i].ball > 0) {
       if(shootDelta > SHOOTDELAY) {
         if(robots[i].heading == 1) {
-          c = new circle(robots[i].x+robots[i].w+10.1,robots[i].y-10.1,7,-20,0,GRAVITY,10,"green");
+          c = new circle(robots[i].x+robots[i].w+10.1,robots[i].y-10.1,getRandom(6,8),-getRandom(18,22),0,GRAVITY,10,robots[i].color);
         }
         else {
-          c = new circle(robots[i].x-10.1,robots[i].y-10.1,-7,-20,0,GRAVITY,10,"green");
+          c = new circle(robots[i].x-10.1,robots[i].y-10.1,-getRandom(6,8),-getRandom(18,22),0,GRAVITY,10,robots[i].color);
         }
         balls.push(c);
         robots[i].ball--;
-        shootTimer = Date.now();
+        robots[i].shootTimer = Date.now();
       }
     }
   }
   for (var i = 0; i < balls.length; i++) {
-
-    if(collisionr(balls[i],robots[0])) {
+    if(collisionr(balls[i],robots[0]) && balls[i].color===robots[0].color) {
       robots[0].ball++;
+      balls.splice(i,1);
+      i--;
+    }
+    else if(collisionr(balls[i],robots[1]) && balls[i].color===robots[1].color) {
+      robots[1].ball++;
       balls.splice(i,1);
       i--;
     }
     else {
       if(collisionr(balls[i],goal)) {
         score += 5;
-        if(balls[i].x<goal.x+goal.w/2) {
-          balls[i].vx = -balls[i].vy;
-          balls[i].vy = -balls[i].vx;
-        }
-        else {
-          balls[i].vx = balls[i].vy;
-          balls[i].vy = balls[i].vx;
-        }
-        //balls.splice(i,1);
-        //i--;
       }
+      if(balls[i].x<goal.x+goal.w/2&&collisionl(balls[i])) {
+        balls[i].vx = -balls[i].vy*BOUNCE_CONSTANT;
+        balls[i].vy = balls[i].vx*BOUNCE_CONSTANT*0.8;
+      }
+      else if(collisionl(balls[i])){
+        balls[i].vx = balls[i].vy*BOUNCE_CONSTANT;
+        balls[i].vy = -balls[i].vx*BOUNCE_CONSTANT*0.8;
+      }
+
       if(balls[i].vx > 0) {
         balls[i].vx -= BALL_FRICTION;
         if(balls[i].vx < 0) {
@@ -229,10 +271,6 @@ function animate()
         if(collision(balls[i], balls[j])) {
           var tevy = balls[i].vy
           var tevx = balls[i].vx;
-          /*balls[i].vy = balls[j].vy - 0.2*balls[i].vy;
-          balls[j].vy = tevy - 0.2*balls[j].vy;
-          balls[i].vx = balls[j].vx - 0.2*balls[i].vx;
-          balls[j].vx = tevx - 0.2*balls[j].vx;*/
           balls[i].vy = balls[j].vy*COLLISION_CONSTANT;
           balls[j].vy = tevy*COLLISION_CONSTANT;
           balls[i].vx = balls[j].vx*COLLISION_CONSTANT;
@@ -241,14 +279,20 @@ function animate()
       }
       balls[i].x += balls[i].vx;
       balls[i].y += balls[i].vy;
-      /*for(var j = i+1; j < balls.length; j++) {
+      for(var j = i+1; j < balls.length; j++) {
         if(collision(balls[i], balls[j])) {
-          balls[i].x = 2*RADIUS*Math.cos(angle(balls[i],balls[j]))+balls[j].x;
-          balls[i].y = 2*RADIUS*Math.sin(angle(balls[i],balls[j]))+balls[j].y;
-
+          //balls[i].x = 2*RADIUS*Math.cos(angle(balls[i],balls[j]))+balls[j].x;
+          //balls[i].y = 2*RADIUS*Math.sin(angle(balls[i],balls[j]))+balls[j].y;
+          if(balls[i].x > balls[j].x) {
+            balls[i].vx += 5;
+            balls[j].vx -= 5;
+          }
+          else {
+            balls[i].vx -= 5;
+            balls[j].vx += 5;
+          }
         }
-      }*/
-
+      }
       balls[i].draw();
     }
   }
@@ -257,37 +301,69 @@ function animate()
 $(document.body).keydown(function (evt) {
   var code = evt.keyCode;
   if (code === 87) { //w key
-    up = true;
+    robots[0].up = true;
   }
   if (code === 83) { //s key
-    down = true;
+    robots[0].down = true;
   }
   if (code === 68) { //d key
-    right = true;
+    robots[0].right = true;
   }
   if (code === 65) { //a key
-    left = true;
+    robots[0].left = true;
   }
   if (code === 69) { //e key
-    shoot = true;
+    robots[0].shoot = true;
+  }
+
+  if (code === 73) { //i key
+    robots[1].up = true;
+  }
+  if (code === 75) { //k key
+    robots[1].down = true;
+  }
+  if (code === 76) { //l key
+    robots[1].right = true;
+  }
+  if (code === 74) { //j key
+    robots[1].left = true;
+  }
+  if (code === 79) { //o key
+    robots[1].shoot = true;
   }
 });
 $(document.body).keyup(function (evt) {
   var code = evt.keyCode
   if (code === 87) { //up key
-    up = false;
+    robots[0].up = false;
   }
   if (code === 83) { //down key
-    down = false;
+    robots[0].down = false;
   }
   if (code === 68) { //right key
-    right = false;
+    robots[0].right = false;
   }
   if (code === 65) { //left key
-    left = false;
+    robots[0].left = false;
   }
   if (code === 69) {
-    shoot = false;
+    robots[0].shoot = false;
+  }
+
+  if (code === 73) { //i key
+    robots[1].up = false;
+  }
+  if (code === 75) { //k key
+    robots[1].down = false;
+  }
+  if (code === 76) { //l key
+    robots[1].right = false;
+  }
+  if (code === 74) { //j key
+    robots[1].left = false;
+  }
+  if (code === 79) { //o key
+    robots[1].shoot = false;
   }
 });
 
@@ -301,10 +377,16 @@ $(function() {
   goal = new rectangle(600,350,100,10,0,0,"red");
   goal.draw();
   robo = new rectangle(60,60,40,40,0,0,"blue");
+  robo2 = new rectangle(CWIDTH-100,60,40,40,0,0,"red");
   robots.push(robo);
-  b = new circle(CWIDTH-60,60,0,0,0,GRAVITY,10,"green");
-  c = new circle(CWIDTH-100,60,0,0,0,GRAVITY,10,"green");
+  robots.push(robo2);
+  a = new circle(CWIDTH-60,60,0,0,0,GRAVITY,10,"blue");
+  b = new circle(CWIDTH-100,60,0,0,0,GRAVITY,10,"blue");
+  c = new circle(60,60,0,0,0,GRAVITY,10,"red");
+  d = new circle(100,60,0,0,0,GRAVITY,10,"red");
+  balls.push(a);
   balls.push(b);
   balls.push(c);
+  balls.push(d);
   raf = window.requestAnimationFrame(animate);
 });
