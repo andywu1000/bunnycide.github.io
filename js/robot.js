bcba5c5c6bd16f02d00aa0457355898234e1bac4
Aaron Zhang <AaronZhang@MacBook-Air.local>
2017-06-06 22:32:39 -0700

    better

--- js/robot.js
+++ js/robot.js
@@ -198,13 +198,13 @@ function animate()
       // }
       //checks if robot on top of other robot
       //change
-      if((robots[1].x-robots[0].x - robots[0].w ) <= 10){ //rightside 0
-        robots[0].x -= 10;
-        robots[1].x += 10;
+      if((robots[1].x-robots[0].x - robots[0].w) <= 10 && (robots[1].x-robots[0].x - robots[0].w) >= 0){ //rightside 0
+        robots[0].x -= 20;
+        robots[1].x += 20;
       }
-      if((robots[0].x - robots[1].x - robots[1].w) <= 10){ //leftside 0
-        robots[0].x += 10;
-        robots[1].x -= 10;
+      if((robots[0].x - robots[1].x - robots[1].w) <= 10 && (robots[0].x - robots[1].x - robots[1].w) >= 0){ //leftside 0
+        robots[0].x += 20;
+        robots[1].x -= 20;
       }
 
 
